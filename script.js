f290ac5a9545584c9d5883b74a808f799aba765f
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-09-20 23:16:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var studentName = "Alexander Aultman";
+var studentAge = 22;
+var isEnrolled = true;
+console.log(studentName, studentAge, isEnrolled);
+String;theDrill = "Hello World!";
+Number:luckyPrime= 13;
+Boolean;lie = true;
+Array;numPad = [1,2,3,4,5,6,7,8,9,0];
+console.log();
+console.log(theDrill, luckyPrime, lie, numPad);
+console.log();
+const statements = {
+    statement1: "This statement is false.",
+    statement2: "That statement is true.",
+    statement3: "That statement is false.",
+    statement4: "These statements are contained in an object called statements"
+}
+    console.log(statements.statement1);
+    console.log(statements.statement2);
+    console.log(statements.statement3);
+    console.log(statements.statement4);
+let x=1;
+let y=2;
+let z=3;
+console.log("x=1,y=2,z=3");
+console.log("if x*y<z, then ++y)");
+if (x*y<z){
+    ++y
+}
+console.log("x=1, y=3, x=3");
+console.log("if x!=z, then x=y");
+if (x!=z){
+    x=z
+}
+console.log("x=3, y=3, x=3");
+console.log("if x&&y===z, then --x");
+if (z===x&&y){
+    --x
+}
+console.log("x=2, y=3, z=2");
+console.log("if (x||y>z),(x&&y>0),(y>x), then z=y-x");
+if ((x||y>z),(x&&y>0),(y>x)){
+    z=y-x
+}
+console.log("x=2, y=3, z=1");
+console.log("if (x=x/z), output Hello World!")
+if ((x||y>z),(x&&y>0),(y>x)){
+    console.log("Hello world!")
+}
